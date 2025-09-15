984bcec97340d6ec88d0b029b6e23caa37b9873d
Ahmad Aizuddin <aizuddinahmd0408@gmail.com>
2025-09-15 12:47:31 +0800

    list of services

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -1,11 +1,11 @@
-import ListofCourses from "@/components/ListofCourses";
+// import ListofCourses from "@/components/ListofCourses";
 import Navbar from "@/components/navbar";
 
 export default function CoursesPage() {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen bg-[#fef9f6] px-4 py-12">
       <Navbar />
-      <ListofCourses />
+      {/* <ListofCourses /> */}
     </section>
   );
 }
