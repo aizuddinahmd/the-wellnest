d00e1397329b6781248d304e8a80a64f16c9cf31
Ahmad Aizuddin <aizuddinahmd0408@gmail.com>
2025-10-16 12:06:50 +0800

    edited cta

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function CTA() {
   return (
@@ -16,13 +16,19 @@ export default function CTA() {
 
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center min-h-[400px]">
-        <div className="text-4xl font-bold text-white mb-8 text-center font-serif">
-          Book Your Session Today
+        <div className="text-5xl font-bold text-white mb-8 text-center font-serif">
+          Want to rent studio for private event?
+        </div>
+        <div className="text-white text-center text-xl mb-8">
+          From birthday parties to corporate events, we&apos;ve got you covered.
         </div>
         <div className="flex flex-wrap items-center justify-center gap-6">
-          <button className="bg-[#d6a496] text-white rounded-xl px-8 py-4 text-lg font-semibold w-fit hover:bg-[#d6a496]/80 transition-colors hover:cursor-pointer shadow-lg">
-            Book Now
-          </button>
+          <Link
+            href="https://wa.link/6w1x6w"
+            className="bg-[#d6a496] text-white rounded-xl px-8 py-4 text-lg font-semibold w-fit hover:bg-[#d6a496]/80 transition-colors hover:cursor-pointer shadow-lg"
+          >
+            Enquire Now
+          </Link>
         </div>
       </div>
     </section>
